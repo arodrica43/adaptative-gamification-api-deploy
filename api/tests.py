a931c2c3614335a6649fc76a1686400bdd99ec28
Alexº <lexws33@gmail.com>
2020-11-11 12:14:24 +0100

    Deploy

--- api/tests.py
+++ api/tests.py
@@ -1,12 +1,44 @@
-from django.test import TestCase
-
+from django.test import TestCase, client
 # Create your tests here.
-from . import models
+from . import models as mod
+
+class UserModelTest(TestCase):
+
+    def setUp(self):
+        self.test_user = mod.User(email="prueba@prueba.com", username='test_user')
+        self.test_user.save()
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        self.test_user.delete()
+
+class UserClientTest(TestCase):
+
+    def setUp(self):
+        self.client = client.Client()
+
+    def test_get(self):
+        response = self.client.get('/api/')
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_post(self):
+
+        response = self.client.post('/api/users/',{'username':'test_user'})
+        print(response.content)
+        self.assertEqual(response.status_code, 201)
+
+    def tearDawn(self):
+        pass
+    
 
 class LeaderboardTestCase(TestCase):
     def setUp(self):
-        models.Leaderboard.objects.create(title = "TestLeaderboard", length = 4, sort_by = "a")     
+        mod.Leaderboard.objects.create(title = "TestLeaderboard", length = 4, sort_by = "a")     
 
     def test_leaderboard_leadders(self):
         """Function to test leadderboard"""
         pass
+
