401bdfe393145a57b19aeb7df575ca7e9249e23c
Alexº <lexws33@gmail.com>
2020-11-11 13:29:31 +0100

    Deploy

--- api/tests.py
+++ api/tests.py
@@ -2,43 +2,78 @@ from django.test import TestCase, client
 # Create your tests here.
 from . import models as mod
 
-class UserModelTest(TestCase):
+class TestException(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'An exception while executing a test function occurred. {0} '.format(self.message)
+        else:
+            return 'An exception while executing a test function occurred.'
+
+class ModelTest(TestCase):
+
+    instance = None
+
+    def subsetUp(self):
+        pass
 
     def setUp(self):
-        self.test_user = mod.User(email="prueba@prueba.com", username='test_user')
-        self.test_user.save()
+        self.subsetUp()
+        self.instance.save()
 
-    def test_something(self):
+    def suptearDown(self):
         pass
 
     def tearDown(self):
-        self.test_user.delete()
+        self.suptearDown()
+        self.instance.delete()
 
-class UserClientTest(TestCase):
+class ClientTest(TestCase):
+
+    urls = []
+    requests = ["GET", "HEAD", "OPTIONS", "POST", "PUT" ]
+
+    def subsetUp(self):
+        pass
 
     def setUp(self):
+        print("")
+        self.subsetUp()
         self.client = client.Client()
+        self.request_functions = [self.client.get, self.client.head, self.client.options, self.client.post, self.client.put]
+        self.write_data = {}
 
-    def test_get(self):
-        response = self.client.get('/api/')
-        self.assertEqual(response.status_code, 200)
-
-    
-    def test_post(self):
-
-        response = self.client.post('/api/users/',{'username':'test_user'})
-        print(response.content)
-        self.assertEqual(response.status_code, 201)
-
+    def test_http_requests(self):
+        for i in range(len(self.requests)):
+            for url in  self.urls:            
+                httpf =  self.request_functions[i]
+                if(self.requests[i] == "POST" or self.requests[i] == "PUT"):
+                    response = self.request_functions[i](url, self.write_data)
+                else:
+                    response = self.request_functions[i](url)
+                assert(100 < response.status_code and response.status_code < 400)
+                print("Testing " + self.requests[i] + " request on URL '" + url + "'")
+             
     def tearDawn(self):
+        self.urls = []
         pass
-    
 
-class LeaderboardTestCase(TestCase):
-    def setUp(self):
-        mod.Leaderboard.objects.create(title = "TestLeaderboard", length = 4, sort_by = "a")     
+class UserClientTest(ClientTest):
 
-    def test_leaderboard_leadders(self):
-        """Function to test leadderboard"""
-        pass
+    def subsetUp(self):
+        self.write_data = {"username" : "test_user"}
+        self.urls = ["/api/users"]
+
+class GamerClientTest(ClientTest):
+
+    def subsetUp(self):
+        self.write_data = {"username" : "test_user"}
+        self.urls = ["/api/gamers"]
+
+
 
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # A sample Bash script, by Ryan
 git status
+git pull
 git add .
 git commit -m "Deploy"
 git push origin main
