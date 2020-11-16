4533c7b975e15fc90671f247f8963472cfe09827
Alexº <lexws33@gmail.com>
2020-11-16 20:50:02 +0100

    Deploy

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,9 @@
-from django.test import TestCase, client
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser
 # Create your tests here.
 from . import models as mod
+from . import views as vws
+import webbrowser as wb
 
 class TestException(Exception):
     def __init__(self, *args):
@@ -15,71 +18,77 @@ class TestException(Exception):
         else:
             return 'An exception while executing a test function occurred.'
 
-class ModelTest(TestCase):
+# class ModelTest(TestCase):
 
-    instance = None
+#     def subsetUp(self):
+#         pass
 
-    def subsetUp(self):
-        pass
+#     def setUp(self):
+#         self.instance = mod.User(username = "Francisco")
+#         self.subsetUp()
+#         self.instance.save()
+#         print(mod.User.objects.all())
+
+#     def subtearDown(self):
+#         pass
+
+#     def tearDown(self):
+#         self.subtearDown()
+#         self.instance.delete()
+
+# class RequestTest(TestCase):
+
+#     def subsetUp(self):
+#         pass
+
+#     def setUp(self):
+#         self.client = client.Client()
+#         self.url = "/api/users"
+#         self.data = {}
+#         self.mode = self.client.get
+#         self.expected_result = lambda r : (100 < r.status_code and r.status_code < 400)
+#         self.subsetUp() # Here we can change default args
+    
+#     def test_things(self):
+#         self.response = self.mode(self.url, data = self.data)
+#         print(self.response.status_code)
+#         assert(self.expected_result(self.response))
+#         print(self.response.request)
+  
+#     def subtearDown(self):
+#         pass
+
+#     def tearDown(self):
+#         self.subtearDown()
+
+class AGAlgorithmTest(TestCase):
 
     def setUp(self):
-        self.subsetUp()
-        self.instance.save()
-
-    def suptearDown(self):
+        self.factory = RequestFactory()
+        self.user = mod.User.objects.create(username="user_test1")
         pass
+
+    def test_X(self):
+
+        data = {"username":"user_test2"}
+         # Create an instance of a GET request.
+        request = self.factory.post('/api/gamers', data= data)
+
+        # Recall that middleware are not supported. You can simulate a
+        # logged-in user by setting request.user manually.
+        request.user = AnonymousUser()
+        response = vws.GamerViewSet.as_view({'post' : 'create'})(request,data)
+        # Use this syntax for class-based views.
+        print(response)
+        print(mod.Gamer.objects.all())
+        #self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
-        self.suptearDown()
-        self.instance.delete()
-
-class ClientTest(TestCase):
-
-    urls = []
-    requests = ["GET", "HEAD", "OPTIONS", "POST", "PUT" ]
-
-    def subsetUp(self):
         pass
 
-    def setUp(self):
-        print("")
-        self.subsetUp()
-        self.client = client.Client()
-        self.request_functions = [self.client.get, self.client.head, self.client.options, self.client.post, self.client.put]
-        self.write_data = {}
-
-    def test_http_requests(self):
-        for i in range(len(self.requests)):
-            for url in  self.urls:            
-                httpf =  self.request_functions[i]
-                if(self.requests[i] == "POST" or self.requests[i] == "PUT"):
-                    response = self.request_functions[i](url, self.write_data)
-                else:
-                    response = self.request_functions[i](url)
-                assert(100 < response.status_code and response.status_code < 400)
-                print("Testing " + self.requests[i] + " request on URL '" + url + "'")
-             
-    def tearDawn(self):
-        self.urls = []
-        pass
-
-class UserClientTest(ClientTest):
-
-    def subsetUp(self):
-        self.write_data = {"username" : "test_user"}
-        self.urls = ["/api/users"]
-
-class GamerClientTest(ClientTest):
-
-    def subsetUp(self):
-        self.write_data = {"username" : "test_user"}
-        self.urls = ["/api/gamers"]
-
-class GMechanicClientTest(ClientTest):
-
-    def subsetUp(self):
-        self.write_data = {"title" : "test_gmechanic"}
-        self.urls = ["/api/g_mechanics"]
+   
 
 
+      
 
+    
